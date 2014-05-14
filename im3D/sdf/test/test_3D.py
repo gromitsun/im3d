7436cdd9ec44dfef4659f9f6cc77805238089097
John Gibbs <jwgibbs@u.northwestern.edu>
2014-05-14 13:16:36 -0500

    Finished various SDF reinitialization options
    There are options for 2D or 3D, datatype, WENO and subcell reinitialization.

--- im3D/sdf/test/test_3D.py
+++ im3D/sdf/test/test_3D.py
@@ -1,15 +1,23 @@
-#! /opt/local/bin/py27-MacPorts -i
+#! python -i
 import im3D.sdf
 import numpy as np
 import matplotlib.pyplot as plot
 
-d1 = 200
-d2 = 65
+d1 = 64
+d2 = 20
 a = np.ones((d1,d1,d1))
 a[d2:-d2,d2:-d2,d2:-d2] = -1
 
 b = im3D.sdf.reinit(a, band=50, verbose=0)
 
-plot.imshow(b[...,20])
-plot.contour(b[...,20], levels=np.linspace(-10,10,5))
+fig_num = 0
+for dtype in ('float32', 'float64'):
+    for subcell in (False, True):
+        for WENO in (False, True):
+            fig_num += 1
+            print('fig: %i | dtype: %s | subcell: %5s | WENO: %5s' %(fig_num, dtype, subcell, WENO))
+            b = im3D.sdf.reinit(a.astype(dtype), dt=0.40, it=25, subcell=subcell, WENO=WENO, verbose=0)
+            plot.figure(fig_num)
+            plot.imshow(b[:,:,d1/2], vmin=-10, vmax=+10)
+            plot.contour(b[:,:,d1/2], levels=np.linspace(-10,10,11), colors='r')
 
