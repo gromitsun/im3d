a255d33d0adf6c48aaaa9db007cd0849be524c36
John Gibbs <jwgibbs@u.northwestern.edu>
2014-05-14 10:35:12 -0500

    Added to the description of the package

--- im3D/__init__.py
+++ im3D/__init__.py
@@ -1,12 +1,10 @@
 """
 A package for processing and visualizing 3D image data
 
-This package contains tools for manipulating image-like data 
-(e.g. creating a signed distance function or calculating spatial 
-gradients)
-
-It also contains tools for visualizing data.  Examples of this 
-include histograms and interfacial shape distributions (ISD)
+This package contains tools for manipulating image-like data (e.g. 
+creating a signed distance function or calculating spatial gradients). 
+It also contains tools for visualizing data.  Examples of this include 
+histograms and interfacial shape distributions (ISD).
 """
 
 import im3D.curvature
